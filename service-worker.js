e87493c2790be091d03a9cd58fbae9ebfd1f34bd
Derek <derekalia@me.com>
2019-05-11 01:43:33 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.dcae6b85d1e44498c877736f4e9e70bc.js"
 );
 
 self.addEventListener('message', (event) => {
