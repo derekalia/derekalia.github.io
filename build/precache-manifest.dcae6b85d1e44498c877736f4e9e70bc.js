d8ac963e48a42142db0c6cd75b2f42afea32445e
Derek <derekalia@me.com>
2019-05-11 01:30:40 -0700

    add build

---
+++ build/precache-manifest.dcae6b85d1e44498c877736f4e9e70bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76f5b805d52cc201a97b582848c9523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f069a13cf780bd28bbcb",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "13061aea33614b49d57b",
+    "url": "/static/js/2.2bb79f13.chunk.js"
+  },
+  {
+    "revision": "f069a13cf780bd28bbcb",
+    "url": "/static/js/main.5eb65fe8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a4c32417ad07f94e519e080e044bc660",
+    "url": "/static/media/github.a4c32417.svg"
+  },
+  {
+    "revision": "1166f7ab679afa5c8cba7dc0430d1231",
+    "url": "/static/media/headshot.1166f7ab.png"
+  },
+  {
+    "revision": "dcde5ccbfc80c3704f02b39673513734",
+    "url": "/static/media/linkedin.dcde5ccb.svg"
+  },
+  {
+    "revision": "d93c877034229cc343d62d1601d99768",
+    "url": "/static/media/medium.d93c8770.svg"
+  },
+  {
+    "revision": "d922b65cf497d2db2d3354a891800fcb",
+    "url": "/static/media/twitter.d922b65c.svg"
+  }
+]);
